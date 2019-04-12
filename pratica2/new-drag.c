c3b4bbb4ba5a51405f9cf8b555777a8faf4ac609
Rodrigo Zago <rodrigozago@me.com>
2019-04-12 11:52:57 -0300

    Adicionando comentarios

--- pratica2/new-drag.c
+++ pratica2/new-drag.c
@@ -5,10 +5,12 @@
 #define WINDOW_WIDTH 400
 #define WINDOW_HEIGHT 400
 
+//definindo tipo da estrutura que representa um ponto
 typedef struct ponto {
     double x, y;
 } Ponto;
 
+//função para criar um ponto
 Ponto criaPonto(double x, double y) {
     Ponto ponto;
 
@@ -18,45 +20,55 @@ Ponto criaPonto(double x, double y) {
     return ponto;
 }
 
+//definição de um quadrado: 4 pontos
 Ponto quadrado[4];
 
+//função que desenha um quadrado
 void desenhaQuadrado(Ponto *quadrado) {
     glColor3f(1, 0, 0);					  //cor vermelha
 	glBegin(GL_QUADS);					  //comeca a desenhar quadrilatros
-    for(int i=0; i < 4; i++) {
+    for(int i=0; i < 4; i++) {            //desenha cada ponto do quadrado
         glVertex2f(quadrado[i].x, quadrado[i].y);
     }
 	glEnd();							  //termina o desenho
 }
 
+//função de exibição
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
+    //desenha o quadrado na tela
     desenhaQuadrado(quadrado);
     glFlush();
 }
 
+//função para retornar um ponto referente ao ponteiro do mouse
 Ponto mouse(int x, int y) {
-    printf("Mouse is on (%f, %f)", (float)(x) / WINDOW_WIDTH, 1 - (float)(y) / WINDOW_HEIGHT);
+    printf("debug: Mouse is on (%f, %f)\n", (float)(x) / WINDOW_WIDTH, 1 - (float)(y) / WINDOW_HEIGHT);
     return criaPonto((float)(x) / WINDOW_WIDTH, 
                      1 - (float)(y) / WINDOW_HEIGHT);
 }
 
+//função para transladar o quadrado para o ponto do ponteiro do mouse
 void moveQuadrado(Ponto *quadrado, Ponto mousePt) {
+    //move primeiro ponto
     quadrado[0].x = mousePt.x - 0.1;
     quadrado[0].y = mousePt.y - 0.1;
     
+    //move segundo ponto
     quadrado[1].x = mousePt.x + 0.1;
     quadrado[1].y = mousePt.y - 0.1;
 
+    //move terceiro ponto
     quadrado[3].x = mousePt.x - 0.1;
     quadrado[3].y = mousePt.y + 0.1;
 
+    //move quarto ponto
     quadrado[2].x = mousePt.x + 0.1;
     quadrado[2].y = mousePt.y + 0.1;
 }
 
-//drag function
-void onMouseMove(int x, int y) {
+//função chamada ao clickar e mover o mouse
+void onMouseClickMove(int x, int y) {
     Ponto mousePt;
     mousePt = mouse(x, y);
 
@@ -71,7 +83,7 @@ void onMouseMove(int x, int y) {
     }
 }
 
-
+//função de inicialização
 void init() {
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
@@ -87,15 +99,27 @@ void init() {
 int main(int argc, char **argv) {
     //função de inicialização openGL
     glutInit(&argc, argv);
+    //utiliza RGB para cores e janela com apenas um buffer
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    //define o tamanho da janela
     glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    //define posição incial da tela
     glutInitWindowPosition(200, 200);
+    //cria janel "Mouse"
     glutCreateWindow("Mouse");
 
-    glutMotionFunc(onMouseMove);
+    //função de movimento, passa como parâmetro o procedimento
+    //a ser executado ao clicar e mover o mouse
+    glutMotionFunc(onMouseClickMove);
 
+    //chama procedimento de inicialização
     init();
+    
+    //passa a função de exibição por parâmetro
     glutDisplayFunc(display);
+    
+    //inicia o loop do glut 
     glutMainLoop();
+
     return 0;
 }
